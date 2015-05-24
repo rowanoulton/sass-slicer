5f4d1115cbf0c66e925417909c43dd4d3f2a0b42
Te Riu Warren <te@summerofdev.com>
2015-05-24 16:03:29 +1200

    Updated CSS indentwidth

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ var sass = require('gulp-sass');
 
 gulp.task('test', function () {
   gulp.src('test/*.scss')
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(sass({outputStyle: 'expanded', indentWidth: '4'}).on('error', sass.logError))
     .pipe(gulp.dest('test/'));
 });
